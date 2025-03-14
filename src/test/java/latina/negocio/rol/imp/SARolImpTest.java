24d76cf746e97b53d0eaae200291f6a673247b52
amaury <amauryav@ucm.es>
2025-03-14 08:31:00 +0100

    cambios de nombres para mayor claridad

--- src/test/java/latina/negocio/rol/imp/SARolImpTest.java
+++ src/test/java/latina/negocio/rol/imp/SARolImpTest.java
@@ -23,36 +23,37 @@ class SARolImpTest {
 
     @Test
     void testNombreRepetido() {
-        // Como son test unitarios, no pueden conectarse con la base de datos ni con nada de eso y se deben crear stubs
-        // para las funciones
+        // Como son test unitarios, no pueden conectarse con la base de datos ni con nada
+        // de eso y se deben crear stubs para las funciones
 
         // Creamos un rol falso que estaria en teoria dentro de la base de datos
-        Rol rolEnBD = new Rol();
-        rolEnBD.setActivo(true);
-        rolEnBD.setNombre("mirol");
-        rolEnBD.setSalario(10);
+        Rol stubRol = new Rol();
+        stubRol.setActivo(true);
+        stubRol.setNombre("mirol");
+        stubRol.setSalario(10);
+
         // Lo metemos en una lista porque es lo que devuelve la query
-        List<Object> resultList = new ArrayList<>();
-        resultList.add(rolEnBD);
+        List<Object> stubResultList = new ArrayList<>();
+        stubResultList.add(stubRol);
 
         // Creamos una query falsa que cuando se le pida los resultados devuelva la lista que creamos antes
-        Query buscarPorNombre = mock(Query.class);
-        when(buscarPorNombre.getResultList()).thenReturn(resultList);
+        Query stubQueryBuscarPorNombre = mock(Query.class);
+        when(stubQueryBuscarPorNombre.getResultList()).thenReturn(stubResultList);
 
         // Creamos una transaccion falsa para devolver cuando se pida al EM
-        EntityTransaction transaction = mock(EntityTransaction.class);
+        EntityTransaction stubTransaction = mock(EntityTransaction.class);
 
         // Creamos un EntityManager falso que al pedirle la query findByNombre devuelva la query falsa de arriba
         // y cuando se le pida la transaccion devuelva la falsa
-        EntityManager entityManager = mock(EntityManager.class);
-        when(entityManager.createNamedQuery("Rol.findBynombre")).thenReturn(buscarPorNombre);
-        when(entityManager.getTransaction()).thenReturn(transaction);
+        EntityManager stubEntityManager = mock(EntityManager.class);
+        when(stubEntityManager.createNamedQuery("Rol.findBynombre")).thenReturn(stubQueryBuscarPorNombre);
+        when(stubEntityManager.getTransaction()).thenReturn(stubTransaction);
 
         // Creamos un SA que vamos a espiar para controlar la creacion del entityManager
         SARolImp sa = Mockito.spy(new SARolImp());
 
         // Cuando el SA llame a crearEntityManager, va a devolver el falso
-        doReturn(entityManager).when(sa).crearEntityManager();
+        doReturn(stubEntityManager).when(sa).crearEntityManager();
 
         // Creamos el transfer con el mismo nombre para que de error
         TRol trol = new TRol("mirol", 0, true);
